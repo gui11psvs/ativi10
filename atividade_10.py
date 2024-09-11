317e61267dfedd9df71c07800d7e300002b05339
guilhermelkx7@gmail.com <guilhermelkx7@gmail.com>
2024-09-11 11:07:09 -0300

    atividade10

---
+++ atividade_10.py
@@ -0,0 +1,10 @@
+# Crie um programa que receba um número inteiro e informe se ele é par ou ímpar 
+numero = int(input("Digite um número inteiro: "))
+
+if numero % 2 == 0:
+    
+print(f"O número {numero} é par.")
+
+else:
+
+print(f"O número {numero} é ímpar.")
